f1e3fb246ea65327678f6cbd22432d493fffc62d
vlasevich <vlasentiy@gmail.com>
2016-08-16 17:24:59 +0300

    created MainActivity class and main_layout xml

---
+++ app/src/main/java/com/home/vlas/rmndm/MainActivity.java
@@ -0,0 +1,15 @@
+package com.home.vlas.rmndm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by vlas on 16.08.2016.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
