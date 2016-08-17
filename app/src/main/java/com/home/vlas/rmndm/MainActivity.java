596c9be78701442903f3974cbe8dbf2c90478ed7
vlasevich <vlasentiy@gmail.com>
2016-08-17 10:17:29 +0300

    [RM-2] added toolbar ans search button

--- app/src/main/java/com/home/vlas/rmndm/MainActivity.java
+++ app/src/main/java/com/home/vlas/rmndm/MainActivity.java
@@ -2,14 +2,29 @@ package com.home.vlas.rmndm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-/**
- * Created by vlas on 16.08.2016.
- */
+
 public class MainActivity extends Activity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
